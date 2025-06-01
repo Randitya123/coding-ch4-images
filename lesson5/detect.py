1386778b9c5b9dcc98cb083de7595196532c1145
Randitya123 <82374360+Randitya123@users.noreply.github.com>
2025-06-01 09:42:56 +0200

    Add files via upload

---
+++ lesson5/detect.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+
+dt=cv2.imread("eye.webp",1)
+at=cv2.cvtColor(dt,cv2.COLOR_BGR2GRAY)
+#making the image a little blurry
+blurese=cv2.blur(at,(3,3))
+#detetcting circle
+circ=cv2.HoughCircles(blurese,cv2.HOUGH_GRADIENT,1,12,param1=50,param2=30,minRadius=10,maxRadius=80)
+if circ is not None:
+    circ=np.uint16(np.around(circ))
+    for i in circ[0,:]:
+        pos1,pos2,radiuss=i[0],i[1],i[2]
+        cv2.circle(dt,(pos1,pos2),radiuss,(67,89,234),3)
+        cv2.circle(dt,(pos1,pos2),1,(67,89,234),3)
+        cv2.imshow("TITLE",dt)
+        cv2.waitKey(0)
+        
