febba1371f6e7b62678ea2c32b470562554ab919
Randitya123 <82374360+Randitya123@users.noreply.github.com>
2025-06-09 18:55:31 +0200

    Update lesson 2.py

--- lesson2/lesson 2.py
+++ lesson2/lesson 2.py
@@ -1,55 +1,38 @@
 import cv2
-import numpy as np
-read=cv2.imread("image.jpg")
-reads=cv2.imread("rgb.png")
-#weighted addition
-add=cv2.addWeighted(read,0.8,reads,0.6,0)
-
-cv2.imshow("Pictures",add)
-cv2.waitKey(0)
-
-#weighted sub
-add=cv2.subtract(read,reads)
-cv2.imshow("Picturessssss",add)
-cv2.waitKey(0)
-#resizing image
-read=cv2.imread("image.jpg")
-store=cv2.resize(read,(600,800))
-cv2.imshow("Picturessreressss",store)
-cv2.waitKey(0)
-#erosion effect
-store=np.ones((5,5),np.uint8)
-erroded=cv2.erode(read,store)
-
-cv2.imshow("Pictures",erroded)
-cv2.waitKey(0)
-
-#bluring of an image
-read=cv2.imread("image1.jpg")
-cv2.imshow("Pictures",read)
-cv2.waitKey(0)
-#Gaussian blur
-store=cv2.GaussianBlur(read,(7,7),0)
-cv2.imshow("Pictures",store)
-cv2.waitKey(0)
-
-#median blur
-store=cv2.medianBlur(read,5)
-cv2.imshow("nfgfg",store)
-cv2.waitKey(0)
-
-#bilateral Filter
-store=cv2.bilateralFilter(read,6,1,1)
-cv2.imshow("Pictures",store)
-cv2.waitKey(0)
-
-#bordering an image
-read=cv2.imread("image1.jpg")
-border=cv2.copyMakeBorder(read,10,10,10,10,cv2.BORDER_CONSTANT,value=1)
-cv2.imshow("bjhbuijb",border)
-cv2.waitKey(0)
-
-#reflecting border
-border=cv2.copyMakeBorder(read,10,10,10,10,cv2.BORDER_REFLECT,value=1)
-cv2.imshow("bjhbuijb",border)
-cv2.waitKey(0)
+import os
+from PIL import Image
+os.chdir(r"C:\Users\KIKE\OneDrive\Desktop\Coding ch4\lesson 7\images")
+path=r"C:\Users\KIKE\OneDrive\Desktop\Coding ch4\lesson 7\images"
+avgh=0
+width=0
+numimages=len(os.listdir("."))
+print(numimages)
+#finding mean width and height
+for i in os.listdir("."):
+    img=Image.open(os.path.join(path,i))
+    widths,height=img.size
+    width=width+widths
+    avgh=avgh+height
+width=width//5
+avgh=avgh//5
+print(avgh)
+print(width)
+for i in os.listdir("."):
+    if i.endswith(".jpg") or i.endswith("jpeg"):
+        img=Image.open(os.path.join(path,i))
+        widths,height=img.size
+        print(widths,height)
+        #reszing 
+        re=img.resize((width,avgh),Image.Resampling.LANCZOS)
+        re.save(i,"JPEG",quality=100)
+        print(img.filename.split('\\')[-1],"is resized")
+#video generation
+def vg():
+    nameoffile="video.avi"
+    os.chdir(r"C:\Users\KIKE\OneDrive\Desktop\Coding ch4\lesson 7\images")
+    imagess=[]
+    for i in os.listdir("."):
+        if i.endswith(".jpeg") or i.endswith("jpg"):
+            imagess.append(i)
+    print(imagess)
+vg()
