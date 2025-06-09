65189c8311d14c66a2bcf17947e89f3a38d226b1
Randitya123 <82374360+Randitya123@users.noreply.github.com>
2025-06-09 18:56:29 +0200

    Update slideshow.py

--- lesson 7/slideshow.py
+++ lesson 7/slideshow.py
@@ -7,3 +7,32 @@ avgh=0
 width=0
 numimages=len(os.listdir("."))
 print(numimages)
+#finding mean width and height
+for i in os.listdir("."):
+    img=Image.open(os.path.join(path,i))
+    widths,height=img.size
+    width=width+widths
+    avgh=avgh+height
+width=width//5
+avgh=avgh//5
+print(avgh)
+print(width)
+for i in os.listdir("."):
+    if i.endswith(".jpg") or i.endswith("jpeg"):
+        img=Image.open(os.path.join(path,i))
+        widths,height=img.size
+        print(widths,height)
+        #reszing 
+        re=img.resize((width,avgh),Image.Resampling.LANCZOS)
+        re.save(i,"JPEG",quality=100)
+        print(img.filename.split('\\')[-1],"is resized")
+#video generation
+def vg():
+    nameoffile="video.avi"
+    os.chdir(r"C:\Users\KIKE\OneDrive\Desktop\Coding ch4\lesson 7\images")
+    imagess=[]
+    for i in os.listdir("."):
+        if i.endswith(".jpeg") or i.endswith("jpg"):
+            imagess.append(i)
+    print(imagess)
+vg()
